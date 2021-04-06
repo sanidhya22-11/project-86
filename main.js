9dab109f22bd8dd30bb646053cab66d3aff6e58c
sanidhya22-11 <77880747+sanidhya22-11@users.noreply.github.com>
2021-04-06 21:20:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var canvas= new fabric.Canvas('my_avengers_canvas');
+player_x=10;
+player_y=10;
+block_image_width=30;
+block_image_height=30;
+var player_object="";
+
+function upload_groot()
+{
+fabric.Image.fromURL("player.png",function(Img){
+player_object = Img; player_object.scaleToWidth(150); player_object.scaleToHeight(140);
+player_object.set({ top:player_y, left:player_x });
+canvas.add(player_object)
+});
+}
+function new_image(get_image) { fabric.Image.fromURL(get_image, function(Img) { draw_image_object = Img; draw_image_object.scaleToWidth(draw_image_width);
+draw_image_object.scaleToHeight(draw_image_height);
+draw_image_object.set({ top:player_y, left:player_x });
+canvas.add(draw_image_object);
+}); }
